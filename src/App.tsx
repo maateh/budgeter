1bcc943b4a7e319c4a983504bc71a2a97047e8bd
maateh <mateharak01@gmail.com>
2024-01-04 17:40:14 +0100

    initial app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { Suspense } from "react"
+import { Route, Routes } from "react-router-dom"
+
+// layouts
+import RootLayout from "./layouts/_root/RootLayout"
+import { Home, Transactions, Wishlist } from "./layouts/_root/pages"
+
 const App = () => {
   return (
-    <div>
-      Budgeter
-    </div>
+    <main className="max-container">
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={
+            <Suspense fallback={<>Loading...</>}>
+              <Home />
+            </Suspense>
+          } />
+          <Route path="/transactions" element={
+            <Suspense fallback={<>Loading...</>}>
+              <Transactions />
+            </Suspense>
+          } />
+          <Route path="/wishlist" element={
+            <Suspense fallback={<>Loading...</>}>
+              <Wishlist />
+            </Suspense>
+          } />
+        </Route>
+
+        <Route path="*" element={
+          <Suspense fallback={<>Loading...</>}>
+            <p>Page not found!</p>
+          </Suspense>
+        } />
+      </Routes>
+    </main>
   )
 }
 
