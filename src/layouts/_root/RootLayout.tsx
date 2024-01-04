794046b536e0d4c031dea1c4905d537ab62da946
maateh <mateharak01@gmail.com>
2024-01-04 16:53:04 +0100

    create root layout and its current components

---
+++ src/layouts/_root/RootLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+const RootLayout = () => {
+  return (
+    <>
+      TODO: Navbar
+      <Outlet />
+    </>
+  )
+}
+
+export default RootLayout
---
+++ src/layouts/_root/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Home } from './Home'
+export { default as Transactions } from './Transactions'
+export { default as Wishlist } from './Wishlist'
