e4e273a6ae0aae976ed2a97f17502912140d6cf3
maateh <mateharak01@gmail.com>
2024-01-07 13:51:57 +0100

    style: some light theme colors changed & ThemeSwitcher added to Navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -7,13 +7,16 @@ import { Menu, PiggyBank } from "lucide-react"
 // constants
 import { navLinks } from "@/constants"
 
+// components
+import ThemeSwitcher from "../theme/ThemeSwitcher"
+
 const Navbar = () => {
   const linksRef = useRef<HTMLUListElement>(null)
 
   return (
     <header className="max-container">
-      <nav className="relative my-5 mx-6 py-3 px-7 flex flex-col md:flex-row items-start md:items-center md:justify-between bg-secondary rounded-[2.15rem] md:rounded-full border-b-neutral-800 border-b-[1px]">
-        <div className="w-full flex justify-between items-center">
+      <nav className=" relative mx-5 my-4 mb-2 px-7 py-3 flex flex-col md:flex-row items-start md:items-center md:justify-between bg-secondary rounded-[2.15rem] md:rounded-full">
+        <div className="w-[93.75%] flex justify-between items-center">
           <NavLink to="/" className="icon-wrapper">
             <PiggyBank size={40} strokeWidth={1} />
             <span className="text-xl md:text-2xl font-bold font-heading small-caps overline tracking-wider">
@@ -28,7 +31,7 @@ const Navbar = () => {
           />
         </div>
 
-        <ul ref={linksRef} className="w-full md:w-1/2 max-md:mt-2 flex flex-col md:flex-row justify-center md:justify-end items-center gap-1 md:gap-4 max-md:[&.mobile-closed]:hidden mobile-closed">
+        <ul ref={linksRef} className="w-full md:w-1/2 max-md:mt-2 mr-4 flex flex-col md:flex-row justify-center md:justify-end items-center gap-1 md:gap-4 max-md:[&.mobile-closed]:hidden mobile-closed">
           {navLinks.map(link => (
             <li key={link.label}>
               <NavLink
@@ -41,6 +44,10 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className="absolute right-1.5 border-l-[1px] border-l-background">
+          <ThemeSwitcher />
+        </div>
       </nav>
     </header>
   )
