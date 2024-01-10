ea88a555cafc8c2e4962571b10fbcc2b6ac9d71b
maateh <mateharak01@gmail.com>
2024-01-10 15:31:08 +0100

    shadcn: progress component added + button component customized

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary-foreground text-primary hover:bg-primary-foreground/85",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-border bg-secondary hover:bg-primary hover:text-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -21,14 +21,21 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        sm: "h-9 px-3",
+        lg: "h-11 px-8",
         icon: "h-10 w-10",
+      },
+      border: {
+        default: "rounded-full",
+        sm: "border-sm",
+        md: "border-md",
+        lg: "border-lg",
       },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      border: "default",
     },
   }
 )
@@ -40,11 +47,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, border, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, border, className }))}
         ref={ref}
         {...props}
       />
