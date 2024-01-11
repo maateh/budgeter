92e981c0dfdb5f7eaeca2580915bac28a97ed1cb
maateh <mateharak01@gmail.com>
2024-01-11 12:45:25 +0100

    shadcn: customize progress component

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -2,24 +2,46 @@
 
 import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
+import { VariantProps, cva } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
+const progressVariants = cva(
+  "relative h-4 w-full overflow-hidden rounded-full",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary-foreground hover:bg-primary-foreground/85",
+        positive:
+          "bg-green-500 hover:bg-green-500/85",
+        negative:
+          "bg-red-500 hover:bg-red-500/85",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>,
+    VariantProps<typeof progressVariants> {
+    maxValue?: number;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  ProgressProps
+>(({ className, variant, value, maxValue, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
-      className
-    )}
+    className={cn(progressVariants({ variant, className }))}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      className="h-full w-full flex-1 bg-neutral-400 transition-all"
+      style={{ transform: `translateX(${variant === 'negative' ? '-' : ''}${(value || 0) / (maxValue || 100) * 100}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
