5e36dfe1354e945d1a8514a434e2d323891b9038
maateh <mateharak01@gmail.com>
2024-01-17 16:47:31 +0100

    schemas were chosen separately from validations

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,39 +1,16 @@
 import * as z from "zod"
 
-import { BudgetType } from "@/models/Budget"
+import { budgetSchema, transactionSchema } from "@/lib/validation/schemas"
 
-export const BudgetValidation = z.object({
-  id: z.string().uuid(),
-  name: z.string().min(2, { message: 'Too short.' }).max(50, { message: 'Too long.' }),
-  type: z.nativeEnum(BudgetType),
-  balance: z.object({
-    current: z.coerce.number()
-      .gte(0)
-      .lte(0)
-      .default(0),
-    ceiling: z.coerce.number()
-  }),
-  theme: z.object({
-    background: z.string()
-      .length(7, { message: 'Background value should be a valid HEX color. e.g. #f1f1f1' })
-      .regex(/^#/),
-    foreground: z.string()
-    .length(7, { message: 'Foregound value should be a valid HEX color. e.g. #f1f1f1' })
-    .regex(/^#/),
+export const BudgetValidation = budgetSchema
+
+export const TransactionValidation = transactionSchema.and(
+  z.object({
+    budgetId: z.string().uuid()
   })
-})
+)
 
-export const TransactionValidation = z.object({
+export const TransactionsValidation = z.object({
   budgetId: z.string().uuid(),
-  id: z.string().uuid(),
-  amount: z.coerce.number(),
-  date: z.date()
-  // TODO: add multiple transactions at once
-  // transactions: z.array(
-  //   z.object({
-  //     id: z.string().uuid(),
-  //     amount: z.coerce.number(),
-  //     date: z.date(),
-  //   })
-  // )
+  transactions: z.array(transactionSchema)
 })
