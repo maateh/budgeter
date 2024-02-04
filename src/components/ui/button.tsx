7fcc320b04d611ad69295c0c89637638fc17969c
maateh <mateharak01@gmail.com>
2024-02-04 15:43:39 +0100

    make button outline great again

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,15 +5,14 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap text-md font-medium font-heading ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap text-md font-heading ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "bg-primary-foreground text-primary hover:bg-primary-foreground/85",
         destructive:
           "bg-destructive text-destructive-foreground/90 font-semibold hover:bg-destructive/90",
-        outline:
-          "border-2 border-border bg-secondary hover:bg-secondary/85 hover:text-foreground underline",
+        outline: "border-2 border-border hover:bg-primary/50 hover:text-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-primary/15 hover:text-accent-foreground",
