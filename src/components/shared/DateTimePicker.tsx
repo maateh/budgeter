46701e5057eca6584865f924e6078a43de326638
maateh <mateharak01@gmail.com>
2024-02-04 12:40:08 +0100

    added: DateTimePicker component + calendar component style changes

---
+++ src/components/shared/DateTimePicker.tsx
@@ -0,0 +1,44 @@
+import { format } from "date-fns"
+import { SelectSingleEventHandler } from "react-day-picker"
+
+// icons
+import { CalendarIcon } from "lucide-react"
+
+// shadcn
+import { Button } from "@/components/ui/button"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+
+type DateTimePickerProps = {
+  label?: string
+  selected: Date
+  onSelect: SelectSingleEventHandler
+}
+
+const DateTimePicker = ({ label = "Pick a date", selected, onSelect }: DateTimePickerProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" className="icon-wrapper">
+          {selected ? (
+            format(selected, 'PPP')
+          ) : (
+            <span>{label}</span>
+          )}
+          <CalendarIcon />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        {/* TODO: implement time picker */}
+        <Calendar
+          mode="single"
+          selected={selected}
+          onSelect={onSelect}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export default DateTimePicker
