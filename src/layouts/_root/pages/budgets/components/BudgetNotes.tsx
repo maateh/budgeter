f31080399e0f6033ccb6b05c220c6c76a9d5cb19
maateh <mateharak01@gmail.com>
2024-02-09 13:46:07 +0100

    creating a ui for adding budget notes

--- src/layouts/_root/pages/budgets/components/BudgetNotes.tsx
+++ src/layouts/_root/pages/budgets/components/BudgetNotes.tsx
@@ -1,46 +1,119 @@
-// types
-import Budget from "@/models/Budget"
+import { useState } from "react"
+
+// icons
+import { MessageCirclePlus } from "lucide-react"
+
+// shadcn
+import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
+import { Label } from "@/components/ui/label"
 
 // components
 import NoteList from "./NoteList"
+
+// types
+import Budget from "@/models/Budget"
 
 type BudgetNotesProps = {
   budget: Budget
 }
 
 const BudgetNotes = ({ budget }: BudgetNotesProps) => {
+  const [editingMode, setEditingMode] = useState(false)
+
+  const handleAddNote = () => {
+    // TODO: add note to budget
+  }
+
   return (
-    <>
-      <h2 className="mb-5">
-        Budget <span className="overline text-blue-500">Notes</span>
-      </h2>
+    <div className="flex flex-col gap-y-7">
+      <div className="flex justify-between items-center gap-x-4">
+        <h2>
+          Budget <span className="overline text-blue-500">Notes</span>
+        </h2>
+
+        <Button
+          className="border-md icon-wrapper"
+          onClick={() => setEditingMode((mode) => !mode)}
+        >
+          <MessageCirclePlus size={20} />
+          <span>Add a Note</span>
+        </Button>
+      </div>
+
+      {editingMode && (
+        <div className="w-3/4 mx-auto flex flex-col gap-1.5">
+          <div className="flex justify-between items-center gap-x-5">
+            <Label className="text-md font-heading">Note Message</Label>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={handleAddNote}
+            >
+              Add
+            </Button>
+          </div>
+          <Textarea placeholder="Type your note here." />
+        </div>
+      )}
 
       <div className="flex flex-col gap-y-10">
-        <div>
+        <div className="flex flex-col gap-y-3.5">
           <h3 className="pl-2 border-l-2 border-green-800 dark:border-green-500">
             <span className="text-green-700 dark:text-green-400">Open</span> Notes
           </h3>
           <NoteList
-            notes={
-              Object.values(budget.notes)
-                .filter(note => !note.date.closed)
-            }
+            budget={budget}
+            // notes={
+            //   Object.values(budget.notes)
+            //     .filter(note => !note.date.closed)
+            // }
+            notes={[
+              {
+                id: '1',
+                text: `Lorem ipsum -
+                dolor sit amet consectetur adipisicing elit. 
+                Eveniet ipsum modi illum repudiandae officia, incidunt vel error reprehenderit aliquam molestiae voluptatibus autem nesciunt quod possimus inventore magni eos accusamus sapiente!`,
+                date: {
+                  created: new Date()
+                }
+              },
+              {
+                id: '2',
+                text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsum modi illum repudiandae officia, incidunt vel error reprehenderit aliquam molestiae voluptatibus autem nesciunt quod possimus inventore magni eos accusamus sapiente!',
+                date: {
+                  created: new Date(),
+                  edited: new Date()
+                }
+              }
+            ]}
           />
         </div>
 
-        <div>
+        <div className="flex flex-col gap-y-3.5">
           <h3 className="pl-2 border-l-2 border-red-700 dark:border-red-500">
             <span className="text-red-600 dark:text-red-400">Closed</span> Notes
           </h3>
           <NoteList
-            notes={
-              Object.values(budget.notes)
-                .filter(note => note.date.closed)
-            }
+            budget={budget}
+            // notes={
+            //   Object.values(budget.notes)
+            //     .filter(note => note.date.closed)
+            // }
+            notes={[
+              {
+                id: '1',
+                text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsum modi illum repudiandae officia, incidunt vel error reprehenderit aliquam molestiae voluptatibus autem nesciunt quod possimus inventore magni eos accusamus sapiente!',
+                date: {
+                  created: new Date(),
+                  closed: new Date()
+                }
+              }
+            ]}
           />
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/layouts/_root/pages/budgets/components/NoteList.tsx
+++ src/layouts/_root/pages/budgets/components/NoteList.tsx
@@ -11,7 +11,7 @@ type NoteList = {
 
 const NoteList = ({ budget, notes }: NoteList) => {
   return notes && notes.length ? (
-    <ul className="mt-3.5 w-full px-2 flex flex-wrap items-center justify-center gap-x-12 gap-y-4">
+    <ul className="w-full px-2 flex flex-wrap items-center justify-center gap-x-12 gap-y-4">
       {notes.map((note) => (
         <li key={note.id}>
           <Note budget={budget} note={note} />
