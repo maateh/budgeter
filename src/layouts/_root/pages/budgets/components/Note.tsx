91a58986905d6df01a58dd5a5eac121c86d9d51d
maateh <mateharak01@gmail.com>
2024-02-09 12:12:45 +0100

    style: designing Note & NoteList components

--- src/layouts/_root/pages/budgets/components/Note.tsx
+++ src/layouts/_root/pages/budgets/components/Note.tsx
@@ -1,31 +1,94 @@
 import { format } from "date-fns"
 
 // icons
-import { CalendarPlus, PenLine } from "lucide-react"
+import { CalendarPlus, CheckCircle, PenLine, PenSquare, Undo2 } from "lucide-react"
+
+// shadcn
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 
 // types
-import { BudgetNote } from "@/models/Budget"
+import Budget, { BudgetNote } from "@/models/Budget"
 
 type NoteProps = {
+  budget: Budget
   note: BudgetNote
 }
 
-const Note = ({ note }: NoteProps) => {
+const Note = ({ budget, note }: NoteProps) => {
+  const handleClose = () => {
+    // TODO: close note
+  }
+
+  const handleEdit = () => {
+    // TODO: implement note editing mode
+  }
+
   return (
-    <>
-      <p>{note.text}</p>
-      {note.date.edited ? (
-        <div className="icon-wrapper">
-          <p>{format(note.date.edited, 'PPP')}</p>
-          <PenLine />
+    <div
+      className="my-1 px-7 pt-6 pb-3 flex flex-col gap-y-2 bg-foreground/15 border-xl border-b-2 border-r-4 hover:opacity-95"
+      style={{
+        borderColor: budget.theme.background,
+        opacity: note.date.closed ? 0.65 : 1
+      }}
+    >
+      <p className="font-medium whitespace-pre-line">{note.text}</p>
+
+      <Separator />
+
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-x-2">
+          <Button
+            variant="icon"
+            size="icon-sm"
+            className="bg-primary border-4 border-primary/60"
+            onClick={handleClose}
+          >
+            {!note.date.closed ? (
+              <CheckCircle
+                size={16}
+                strokeWidth={2.5}
+                className="text-green-600 dark:text-green-400"
+              />
+            ) : (
+              <Undo2
+                size={16}
+                strokeWidth={2.5}
+                className="text-orange-600 dark:text-orange-400"
+              />
+            )}
+          </Button>
+          
+          <Button
+            variant="icon"
+            size="icon-sm"
+            className="bg-primary border-4 border-primary/60"
+            onClick={handleEdit}
+            disabled={!!note.date.closed}
+          >
+            <PenSquare
+              size={16}
+              strokeWidth={2.5}
+              className="text-accent"
+            />
+          </Button>
         </div>
-      ) : (
-        <div className="icon-wrapper">
-          <p>{format(note.date.created, 'PPP')}</p>
-          <CalendarPlus size={18} strokeWidth={1.7} />
+
+        <div className="font-heading all-small-caps icon-wrapper">
+          {note.date.edited ? (
+            <>
+              <p>{format(note.date.edited, 'PPP')}</p>
+              <PenLine />
+            </>
+          ) : (
+            <>
+              <p>{format(note.date.created, 'PPP')}</p>
+              <CalendarPlus size={18} strokeWidth={1.7} />
+            </>
+          )}
         </div>
-      )}
-    </>
+      </div>
+    </div>
   )
 }
 
--- src/layouts/_root/pages/budgets/components/NoteList.tsx
+++ src/layouts/_root/pages/budgets/components/NoteList.tsx
@@ -1,24 +1,25 @@
 // types
-import { BudgetNote } from "@/models/Budget"
+import Budget, { BudgetNote } from "@/models/Budget"
 
 // components
 import Note from "./Note"
 
 type NoteList = {
+  budget: Budget
   notes: BudgetNote[]
 }
 
-const NoteList = ({ notes }: NoteList) => {
+const NoteList = ({ budget, notes }: NoteList) => {
   return notes && notes.length ? (
-    <ul className="w-full px-2 flex flex-wrap items-center justify-center gap-x-12 gap-y-4">
+    <ul className="mt-3.5 w-full px-2 flex flex-wrap items-center justify-center gap-x-12 gap-y-4">
       {notes.map((note) => (
         <li key={note.id}>
-          <Note note={note} />
+          <Note budget={budget} note={note} />
         </li>
       ))}
     </ul>
   ) : (
-    <pre className="text-muted">There are no recorded notes currently.</pre>
+    <pre className="text-muted truncate">There are no recorded notes currently.</pre>
   )
 }
 
