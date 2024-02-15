3dfdaf5734a20f4ab77767c031f5f00f49ebacaf
maateh <mateharak01@gmail.com>
2024-02-15 13:59:34 +0100

    removing unused stuffs

--- src/components/form/transaction/hooks/index.ts
+++ src/components/form/transaction/hooks/index.ts
@@ -1,4 +1,3 @@
-export { default as useTransactionForm } from './useTransactionForm'
 export { default as useTransactionSubmit } from './useTransactionSubmit'
 export { default as useTransferringTransactionSubmit } from './useTransferringTransactionSubmit'
 export { default as useTemporaryTransactionSubmit } from './useTemporaryTransactionSubmit'
