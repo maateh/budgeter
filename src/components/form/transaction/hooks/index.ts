22d476f79a8360c4f2dbc01321862827dc322114
maateh <mateharak01@gmail.com>
2024-02-19 10:50:16 +0100

    remove exports

--- src/components/form/transaction/hooks/index.ts
+++ src/components/form/transaction/hooks/index.ts
@@ -1,5 +1,4 @@
 export { default as useDefaultValues } from './useDefaultValues'
-export { default as useSaveTransactionMutation } from './useSaveTransactionMutation'
 export { default as useTransactionSubmit } from './useTransactionSubmit'
 export { default as useTransferringTransactionSubmit } from './useTransferringTransactionSubmit'
 export { default as useTemporaryTransactionSubmit } from './useTemporaryTransactionSubmit'
