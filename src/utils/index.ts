a616592c6d8f665b218d6d8fc80306a68ec858e3
maateh <mateharak01@gmail.com>
2024-03-10 18:10:14 +0100

    update: currency formatter changes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 export function formatWithCurrency(amount: number, currency: string) {
   return new Intl.NumberFormat(navigator.language || 'en-UK', {
     style: 'currency',
+    currencyDisplay: 'narrowSymbol',
+    trailingZeroDisplay: 'stripIfInteger',
     currency
   }).format(amount)
 }
