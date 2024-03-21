264aa62e994aabca5233f4fd1df74bece3c232d2
maateh <mateharak01@gmail.com>
2024-03-21 18:21:52 +0100

    create data-table with selection & pagination options

--- src/_root/pages/backup/components/BackupTable.tsx
+++ src/_root/pages/backup/components/BackupTable.tsx
@@ -1,14 +1,37 @@
+import { ColumnDef } from "@tanstack/react-table"
+
 // icons
 import { PackageCheck, PackageOpen } from "lucide-react"
 
 // shadcn
 import { Button } from "@/components/ui/button"
+import { DataTable, DataTablePagination, getSelectionColumn } from "@/components/ui/data-table"
 
 // hooks
+import { useBudgets } from "@/lib/react-query/queries"
 import { useCreateBackup } from "@/lib/react-query/mutations"
 
+// types
+import { Budget } from "@/services/api/types"
+
+const columns: ColumnDef<Budget>[] = [
+  getSelectionColumn(),
+  {
+    accessorKey: "name",
+    header: "Budget Name"
+  },
+  {
+    accessorKey: "balance.current",
+    header: "Balance"
+  },
+  {
+    accessorKey: "type",
+    header: "Type"
+  }
+]
+
 const BackupTable = () => {
-  // WIP
+  const { data: budgets, isLoading: isBudgetsLoading } = useBudgets()
   const { data: url, isPending: isBackupPending, mutateAsync: createBackup } = useCreateBackup()
 
   const handleCreate = async () => {
@@ -19,28 +42,34 @@ const BackupTable = () => {
     }
   }
 
-  // TODO: budget selector (table) with actions in a footer
-  return (
-    <div>
-      <Button className="w-fit icon-wrapper"
-        onClick={handleCreate}
-      >
-        <PackageOpen />
-        Create a Backup
-      </Button>
+  return !isBudgetsLoading && budgets && (
+    <DataTable columns={columns} data={budgets}>
+      {(table) => (
+        <>
+          <DataTablePagination table={table} showOnlyIfRequired />
 
-      {!isBackupPending ? url && (
-        <a className="w-fit px-3.5 py-1.5 rounded-full border-2 icon-wrapper"
-          href={url}
-          download
-        >
-          <PackageCheck />
-          Your Backup is ready to save
-        </a>
-      ) : (
-        <>Loading...</> // TODO: skeleton
+          {/* TODO: implement footer with backup actions */}
+          {/* <Button className="w-fit icon-wrapper"
+            onClick={handleCreate}
+          >
+            <PackageOpen />
+            Create a Backup
+          </Button>
+
+          {!isBackupPending ? url && (
+            <a className="w-fit px-3.5 py-1.5 rounded-full border-2 icon-wrapper"
+              href={url}
+              download
+            >
+              <PackageCheck />
+              Your Backup is ready to save
+            </a>
+          ) : (
+            <>Loading...</> // TODO: skeleton
+          )} */}
+        </>
       )}
-    </div>
+    </DataTable>
   )
 }
 
---
+++ src/components/ui/data-table.tsx
@@ -0,0 +1,160 @@
+import { useState } from "react"
+import { ColumnDef, Table as RTable, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table"
+
+// shadcn
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { ChevronLeftIcon, ChevronRightIcon, ChevronsLeftIcon, ChevronsRightIcon } from "lucide-react"
+
+interface DataTableProps<D, V> {
+  columns: ColumnDef<D, V>[]
+  data: D[]
+  children?: (table: RTable<D>) => React.ReactNode
+}
+
+function getSelectionColumn<D, V>(): ColumnDef<D, V> {
+  return {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(checked) => table.toggleAllPageRowsSelected(!!checked)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(checked) => row.toggleSelected(!!checked)}
+        aria-label="Select row"
+      />
+    )
+  }
+}
+
+function DataTable<D, V>({ data, columns, children }: DataTableProps<D, V>) {
+  const [rowSelection, setRowSelection] = useState({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: { rowSelection }
+  })
+
+  return (
+    <>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {!header.isPlaceholder && flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(
+                    cell.column.columnDef.cell,
+                    cell.getContext()
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          )) : (
+            <TableRow>
+              <TableCell className="h-24 text-center"
+                colSpan={columns.length}
+              >
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      {children && children(table)}
+    </>
+  )
+}
+
+interface DataTablePaginationProps<D> {
+  table: RTable<D>
+  showOnlyIfRequired?: boolean
+}
+ 
+function DataTablePagination<D>({ table, showOnlyIfRequired }: DataTablePaginationProps<D>) {
+  return !showOnlyIfRequired || (table.getCanPreviousPage() || table.getCanNextPage()) && (
+    <div className="flex items-center justify-between px-2">
+      <div className="flex-1 text-sm text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        {table.getFilteredRowModel().rows.length} row(s) selected.
+      </div>
+      <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          Page {table.getState().pagination.pageIndex + 1} of{" "}
+          {table.getPageCount()}
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button className="hidden h-8 w-8 p-0 lg:flex"
+            variant="outline"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to first page</span>
+            <ChevronsLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button className="h-8 w-8 p-0"
+            variant="outline"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to previous page</span>
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to next page</span>
+            <ChevronRightIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to last page</span>
+            <ChevronsRightIcon className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export {
+  DataTable,
+  DataTablePagination,
+  getSelectionColumn
+}
