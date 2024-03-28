9a596172c365b5e8c46835d25bf48d17d1789e80
maateh <mateharak01@gmail.com>
2024-03-28 19:33:03 +0100

    fix backup validation types

--- src/components/form/backup/validations/index.ts
+++ src/components/form/backup/validations/index.ts
@@ -4,11 +4,12 @@ import { z } from "zod"
 import { balanceSchema, budgetSchema } from "@/components/form/budget/validations"
 import { budgetNoteSchema } from "@/components/form/budget-note/validations"
 import { transactionSchema } from "@/components/form/transaction/validations"
+import { paymentSchema } from "@/components/form/subpayment/validations"
 
 export const backupDataSchema = z.object({
   budgets: z.record(z.string().uuid(),
     budgetSchema.extend({
-      id: z.string().uuid({ message: 'ID is invalid.' }),
+      id: z.string().uuid({ message: 'Budget ID is invalid.' }),
       balance: balanceSchema.extend({
         income: z.coerce.number()
           .gte(0, { message: 'Income must be a positive number.' }),
@@ -19,14 +20,23 @@ export const backupDataSchema = z.object({
   ),
   transactions: z.record(z.string().uuid(),
     transactionSchema.extend({
-      id: z.string().uuid({ message: 'ID is invalid.' }),
-      createdAt: z.coerce.date()
+      id: z.string().uuid({ message: 'Transaction ID is invalid.' }),
+      createdAt: z.coerce.date(),
+      payment: paymentSchema.extend({
+        id: z.string().uuid({ message: 'Payment ID is invalid.' }),
+        transactionId: z.string().uuid({ message: 'Transaction ID is invalid.' }),
+        paidBackAmount: z.coerce.number().optional()
+      }),
+      subpayments: z.array(paymentSchema.extend({
+        id: z.string().uuid({ message: 'Payment ID is invalid.' }),
+        transactionId: z.string().uuid({ message: 'Transaction ID is invalid.' })
+      })).optional()
     })
   ),
   notes: z.record(z.string().uuid(),
     budgetNoteSchema.extend({
-      id: z.string().uuid({ message: 'ID is invalid.' }),
-      budgetId: z.string().uuid({ message: 'ID is invalid.' }),
+      id: z.string().uuid({ message: 'Note ID is invalid.' }),
+      budgetId: z.string().uuid({ message: 'Budget ID is invalid.' }),
       status: z.union([
         z.literal('open'),
         z.literal('closed')
