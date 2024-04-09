4ded57ab0d28236261452d904f4e5000439c4135
maateh <mateharak01@gmail.com>
2024-04-09 09:46:53 +0200

    update: add columns to TransactionsTable and change pagination

--- src/_root/pages/transactions/components/TransactionsTable.tsx
+++ src/_root/pages/transactions/components/TransactionsTable.tsx
@@ -1,41 +1,83 @@
 // shadcn
-import { DataTable, getSelectionColumn } from "@/components/ui/data-table"
+import { DataTable, DataTablePagination, getSelectionColumn } from "@/components/ui/data-table"
+
+// components
+import BudgetNameBadge from "@/components/shared/budget/custom/BudgetNameBadge"
+import PaymentBadge from "@/components/shared/payment/custom/PaymentBadge"
 
 // hooks
 import { useTransactionsPagination } from "@/lib/react-query/queries"
-import { usePagination } from "@/hooks"
 
 // types
 import { ColumnDef } from "@tanstack/react-table"
 import { Budget, Transaction } from "@/services/api/types"
 
-// TODO: add and design required columns
+// utils
+import { format } from "date-fns"
+
+// TODO: design cells
 const columns: ColumnDef<(Transaction & { budget: Budget })>[] = [
   // TODO: selection is required to bulk delete selected transactions
   getSelectionColumn(),
   {
     accessorKey: "name",
     header: "Name"
+  },
+  {
+    accessorKey: "paymentId",
+    header: "Payment",
+    cell: ({ row }) => (
+      <PaymentBadge
+        size="sm"
+        payment={row.original.payment}
+        currency={row.original.budget.balance.currency}
+        processed={row.original.processed}
+      />
+    )
+  },
+  {
+    accessorKey: "type",
+    header: "Type"
+  },
+  {
+    accessorKey: "budgetId",
+    header: "Budget",
+    cell: ({ row }) => (
+      <BudgetNameBadge
+        size="sm"
+        budget={row.original.budget}
+      />
+    )
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created",
+    cell: ({ row }) => format(row.original.createdAt, 'yyyy. MM. dd.')
+  },
+  {
+    accessorKey: "updatedAt",
+    header: "Last Updated",
+    cell: ({ row }) => format(row.original.updatedAt, 'yyyy. MM. dd.')
+  },
+  {
+    accessorKey: "processed",
+    header: "Processed"
   }
 ]
 
 const TransactionsTable = () => {
   const { data, isLoading, isFetchingNextPage, fetchNextPage } = useTransactionsPagination({
-    params: { limit: 20, offset: 0 }
+    params: { limit: 10, offset: 0 }
   })
-
-  const { observerRef } = usePagination({ data, fetchNextPage })
 
   return !isLoading && data && (
     <>
       <DataTable
         columns={columns}
         data={data.pages.flatMap((page) => page.data)}
-      />
-
-      <div ref={observerRef}>
-        {isFetchingNextPage && <>Loading...</>} {/* TODO: skeleton */}
-      </div>
+      >
+        {(table) => <DataTablePagination table={table} showOnlyIfRequired />}
+      </DataTable>
     </>
   )
 }
