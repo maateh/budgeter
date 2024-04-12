c967874d8c6695ab26e94d559708c8899e7457bd
maateh <mateharak01@gmail.com>
2024-04-12 16:33:44 +0200

    add: FilterInput component

---
+++ src/components/input/FilterInput.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react"
+
+// icons
+import { Filter, FilterX, X } from "lucide-react"
+
+// shadcn
+import { ButtonTooltip } from "@/components/ui/button"
+import { Label, LabelProps } from "@/components/ui/label"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectTriggerProps, SelectValue } from "@/components/ui/select"
+
+// components
+import StateToggle from "@/components/ui/custom/StateToggle"
+
+// types
+import { FilterOptions } from "@/services/api/types"
+
+// utils
+import { cn } from "@/lib/utils"
+
+type SelectOption = {
+  value: string
+  label: string
+}
+
+type FilterInputWithSelectProps<F = unknown> = {
+  options: SelectOption[]
+  value: string
+  setValue: (value: string, filterType: keyof FilterOptions<F>) => void
+  children?: never
+} & SelectTriggerProps
+
+type FilterInputWithoutSelectProps<F = unknown> = {
+  children: (type: keyof FilterOptions<F>) => React.ReactNode
+  options?: never
+  value?: never
+  setValue?: never
+}
+
+type FilterInputProps<F = unknown> = {
+  label: string
+  labelProps?: LabelProps
+  onReset?: () => void
+} & (FilterInputWithSelectProps<F> | FilterInputWithoutSelectProps<F>)
+
+function FilterInput<F>({
+  label, labelProps, onReset,
+  options, value, setValue,
+  children, ...props
+}: FilterInputProps<F>) {
+  const [type, setType] = useState<keyof FilterOptions<F>>('filterBy')
+
+  return (
+    <div className="my-3.5 flex flex-col gap-y-0.5">
+      <Label {...labelProps}
+        className={cn("text-base font-heading font-normal", labelProps?.className)}
+      >
+        Filter by <span className="text-accent capitalize overline">{label}</span>
+      </Label>
+
+      <div className="flex justify-between items-center gap-x-2.5">
+        {children ? children(type) : (
+          <Select
+            value={value}
+            onValueChange={(value) => setValue(value, type)}
+          >
+            <SelectTrigger className={cn(!value && "text-muted-foreground")} {...props}>
+              <SelectValue placeholder="Choose..." />
+            </SelectTrigger>
+            <SelectContent>
+              {options.map(({ value, label }) => (
+                <SelectItem key={value} value={value}>
+                  {label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        )}
+
+        <div className="flex justify-center items-center gap-x-2">
+          <StateToggle
+            status={type === 'filterBy' ? 'on' : 'off'}
+            onClick={() => setType((type) => type === 'filterBy' ? 'excludeBy' : 'filterBy')}
+            icon={{
+              on: <Filter className="text-accent" size={16} strokeWidth={2.5} />,
+              off: <FilterX className="text-destructive" size={16} strokeWidth={2.5} />
+            }}
+            tooltip={{
+              on: "Switch to exclude",
+              off: "Switch to filter"
+            }}
+            toggleOnHover
+          />
+
+          {onReset && (
+            <ButtonTooltip className="p-1"
+              variant="outline"
+              size="icon"
+              onClick={onReset}
+              tooltip="Clear filter"
+            >
+              <X size={14} strokeWidth={3.5} />
+            </ButtonTooltip>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FilterInput
