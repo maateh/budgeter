68ab083d8ae2a0d50a7c79cada70bf1a723091e8
maateh <mateharak01@gmail.com>
2024-05-03 11:19:45 +0200

    fix: useBudget query hook params

--- src/lib/react-query/queries/budget/useBudget.ts
+++ src/lib/react-query/queries/budget/useBudget.ts
@@ -6,12 +6,12 @@ import { useAPI } from "@/services/providers/api/APIContext.hooks"
 // types
 import { Budget } from "@/services/api/types"
 
-const useBudget = (budgetId?: string, options?: Omit<UseQueryOptions<Budget>, 'queryKey' | 'queryFn'>) => {
+const useBudget = (budgetId: string, options?: Omit<UseQueryOptions<Budget>, 'queryKey' | 'queryFn'>) => {
   const { api } = useAPI()
 
   return useQuery({
     queryKey: ['budget', { id: budgetId }],
-    queryFn: async () => await api.budget.getById(budgetId!),
+    queryFn: async () => await api.budget.getById(budgetId),
     ...options
   })
 }
